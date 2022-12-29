08e87444f059b05c76f18dcd5bafa6dd9188c260
Nicolai Brand <nicolahb@stud.ntnu.no>
2022-12-29 22:09:21 +0100

    move source code into one single header (#3)

--- test.c
+++ test.c
@@ -17,20 +17,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "m_arena.h"
-
-
-void test()
-{
-//#ifdef linux
-//    memory = mmap(NULL, GB(2), PROT_NONE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); 
-//#elif defined __unix__
-//    int fd = open("/dev/zero", O_RDWR);   
-//    if (fd == -1)
-//        exit(1);
-//    addr = mmap(NULL, length, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-//#endif
-}
+#define SAC_IMPLEMENTATION
+#include "sac.h"
 
 
 int main()
@@ -38,7 +26,7 @@ int main()
 #ifdef BAD_AARCH
     fprintf(stderr, "may not work properly");
 #endif
-    struct m_arena *arena = m_arena_init(32);
+    struct m_arena *arena = m_arena_init(0, 4096);
 
     char *str = m_arena_alloc(arena, 16);
     char *str2 = m_arena_alloc(arena, 16);
